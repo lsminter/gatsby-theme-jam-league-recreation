5e7a269ad5aebb5a28b1c732e1c5bc8995a51390
lucas <lsminteris@gmail.com>
2019-07-29 19:32:38 -0700

    Remove .cache and node_modules

--- gatsby-theme-events/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-templates-events-js": () => import("/Users/lsminter/Desktop/gatsby-theme-jam/gatsby-theme-events/src/templates/events.js" /* webpackChunkName: "component---src-templates-events-js" */),
-  "component---src-templates-event-js": () => import("/Users/lsminter/Desktop/gatsby-theme-jam/gatsby-theme-events/src/templates/event.js" /* webpackChunkName: "component---src-templates-event-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/lsminter/Desktop/gatsby-theme-jam/gatsby-theme-events/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
-}
-
