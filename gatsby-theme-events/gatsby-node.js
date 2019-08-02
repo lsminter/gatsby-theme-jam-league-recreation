5953d51b2ce126d01f3ba4296bc9fbdb3da84620
lucas <lsminteris@gmail.com>
2019-08-02 14:08:48 -0700

    Edited gatsby-config, edited gatsby-node, added gatsby-config
    Edited gatsby-config to be function, added those changed into gatsby-node, added gatsby-config in site file

--- gatsby-theme-events/gatsby-node.js
+++ gatsby-theme-events/gatsby-node.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 // 1. make sure the data directory exists
 
-exports.onPreBootstrap = ({ reporter }) => {
-  const contentPath = 'data';
+exports.onPreBootstrap = ({ reporter }, options) => {
+  const contentPath = options.contentPath || 'data';
 
   if (!fs.existsSync(contentPath)) {
     reporter.info(`creating the ${contentPath} directory`);
@@ -29,8 +29,8 @@ exports.sourceNodes = ({ actions }) => {
 
 // 3. define resolvers for any custom fields (slug)
 
-exports.createResolvers = ({ createResolvers }) => {
-  const basePath = '/';
+exports.createResolvers = ({ createResolvers }, options) => {
+  const basePath = options.basePath || '/';
 
   const slugify = str => {
     const slug = str
@@ -52,8 +52,8 @@ exports.createResolvers = ({ createResolvers }) => {
 
 // 4. query for events and pages
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const basePath = '/';
+exports.createPages = async ({ actions, graphql, reporter }, options) => {
+  const basePath = options.basePath || '/';
   actions.createPage({
     path: basePath,
     component: require.resolve('./src/templates/events.js')
